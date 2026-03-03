3be932237e40037f470511c45f7a54d9cb20c4e0
AwesomeFaceEnthusiast <epicpenguin133789@gmail.com>
2026-03-03 17:14:53 -0500

    Update code.js

--- africa/code.js
+++ africa/code.js
@@ -4,26 +4,28 @@ var options = [];
 var ids = [];
 var randomOpt;
 
-if (mapID === "bhGovernorates") {
-  options = ["Capital Governorate", "Muharraq Governorate", "Northern Governorate", "Southern Governorate"];
-  ids = ["capitalGovernorate","muharraqGovernorate","northernGovernorate","southernGovernorate"];
-}
-ids = ids.map(id => mapName.contentDocument.getElementById(id));
-randomOpt = options[Math.floor(Math.random()*ids.length)];
-clickText.innerHTML = `Click on ${randomOpt}`;
-
-ids.forEach(id => {
-  id.addEventListener('mouseover', () => {
-    id.style.fill = "#FFFFEF";
-    id.style.cursor = "pointer";
-  });
-  id.addEventListener('mouseout', () => {
-    id.style.fill = "#FFFFEA";
-    id.style.cursor = "auto";
-  });
-  id.addEventListener('mousedown', () => {
-    if (options.indexOf(randomOpt) !== ids.indexOf(id)) {
-      console.log("Wrong");
-    }
+document.addEventListener('load', () => {
+  if (mapID === "bhGovernorates") {
+    options = ["Capital Governorate", "Muharraq Governorate", "Northern Governorate", "Southern Governorate"];
+    ids = ["capitalGovernorate","muharraqGovernorate","northernGovernorate","southernGovernorate"];
+  }
+  ids = ids.map(id => mapName.contentDocument.getElementById(id));
+  randomOpt = options[Math.floor(Math.random()*ids.length)];
+  clickText.innerHTML = `Click on ${randomOpt}`;
+  
+  ids.forEach(id => {
+    id.addEventListener('mouseover', () => {
+      id.style.fill = "#FFFFEF";
+      id.style.cursor = "pointer";
+    });
+    id.addEventListener('mouseout', () => {
+      id.style.fill = "#FFFFEA";
+      id.style.cursor = "auto";
+    });
+    id.addEventListener('mousedown', () => {
+      if (options.indexOf(randomOpt) !== ids.indexOf(id)) {
+        console.log("Wrong");
+      }
+    });
   });
 });
